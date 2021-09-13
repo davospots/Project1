d3344b8fb4b128c16474cc042320cca04bf6fa08
Dave <david.mathaga@student.moringaschool.com>
2021-09-13 10:54:09 +0300

    making design in what we do toggle

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-    $(".para").hide();
+    $("img[alt=design]").on("click",function(){
+        $("img[alt=design").hide();
+        $(".para-one").show();
+    })
+    $(".para-one").on("click",function(){
+        $("img[alt=design").show();
+        $(".para-one").hide();
+    })        
+
+
+    $("img[alt=design]").on("click",function(){
+        $("img[alt=design").hide();
+        $(".para-one").show();
+    })
+    $(".para-one").on("click",function(){
+        $("img[alt=design").show();
+        $(".para-one").hide();
+    })      
+
   });
